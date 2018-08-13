596ceacff2520dde744c703ef672fbb67d98b234
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-08-13 09:46:01 -0400

    added cli, deal, scraper classed and modified daily_deal

--- lib/daily_deal.rb
+++ lib/daily_deal.rb
@@ -1,5 +1,12 @@
-require "daily_deal/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
-module DailyDeal
-  # Your code goes here...
-end
+require_relative "./daily_deal/version"
+require_relative './daily_deal/deal'
+require_relative './daily_deal/cli'
+require_relative './daily_deal/scraper'
+#
+# module DailyDeal
+#   # Your code goes here...
+# end

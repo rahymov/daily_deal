ea61c37d70f5fa4b58dc79469e4ccbf1a1f7f860
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-08-17 09:04:40 -0400

    added .DS_Store to gitignore and fixed cli syntax error

--- lib/daily_deal/cli.rb
+++ lib/daily_deal/cli.rb
@@ -1,7 +1,8 @@
 require 'pry'
 class DailyDeal::CLI
   def call
-    puts "Welcome to Daily Deal Program."
+    puts "Welcome to Groupon Daily Deal Program."
+    "--" * 20
     list_deals
     menu
     goodbye
@@ -15,24 +16,22 @@ class DailyDeal::CLI
 
     @deals.each.with_index(1) do |deal, i|
       puts "#{i}. #{deal.title} "
-      # - #{deal.ratings}
-      # i += 1
     end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the deal you'd like more info on deals or type exit:"
+      puts "Enter the number of the deal you'd like more info on deals or type list, exit:"
       input = gets.strip.downcase
 
       if input.to_i > 0
         the_deal = @deals[input.to_i-1]
-        puts "#{the_deal.title} - (#{the_deal.original_price}) #{the_deal.discount_price} - #{the_deal.bought} - #{the_deal.url} - #{the_deal.ratings}exitratings"
+        puts "#{the_deal.title} - (#{the_deal.original_price}) #{the_deal.discount_price} - #{the_deal.bought} - #{the_deal.url} - #{the_deal.ratings}ratings"
         # binding.pry
       elsif input == "list"
         list_deals
-      else
+      elsif input != "list" || input != "exit"
         puts "Not sure what you want, type list or exit."
       end
     end

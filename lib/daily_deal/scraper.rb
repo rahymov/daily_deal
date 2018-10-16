4ac0c0494b6dcbf7aa949c392775b0e3024ad5d6
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-10-15 20:00:55 -0400

    fix css class name for prices

--- lib/daily_deal/scraper.rb
+++ lib/daily_deal/scraper.rb
@@ -1,5 +1,5 @@
 class DailyDeal::Scraper
-  
+
   def self.scrape_groupon
     puts "*********Scraping******"
     doc = Nokogiri::HTML(open("https://www.groupon.com/occasion/deals-of-the-day"))
@@ -7,8 +7,8 @@ class DailyDeal::Scraper
     deal_parent.each do |figure|
       deal = DailyDeal::Deal.new
       deal.title = figure.css(".cui-udc-title").text.strip
-      deal.original_price = figure.css(".cui-price-original").text.strip
-      deal.discount_price = figure.search("span.cui-price-discount").text.strip
+      deal.original_price = figure.css(".cui-price-original-same-size").text.strip
+      deal.discount_price = figure.search("span.cui-price-discount-same-size").text.strip
       deal.bought = figure.css(".cui-quantity-bought").text.strip
       deal.ratings = figure.css(".cui-review-rating").text.strip
       deal.url = figure.css("a").attr("href")
